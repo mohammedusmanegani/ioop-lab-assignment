daf3a74cee05d05c78cfe420970bab3831a03c5c
Mohammed Usman E Gani <53576076+mohammedusmanegani@users.noreply.github.com>
2021-06-12 17:07:58 +0530

    Update magazine.java

--- magazine.java
+++ magazine.java
@@ -18,50 +18,5 @@ public class magazine {
 		bullet_stack = new Vector<Integer>(size);
 	}
 	
-	/*
-	* Function Name: reFillMagazine
-	* Author: Narasimha
-	* Function Parameter: int
-	* Return Type: Integer
-	* Function Description: This function takes an integer and pushes it on top of the bullet_stack.
-	*/
-	public Integer reFillMagazine(int x) {
-		if(top == size-1) {
-			return 2;
-		}
-		top++;
-		bullet_stack.add(top,x);
-		return 1;
-	}
 	
-	/*
-	* Function Name: fireBullet
-	* Author: Narasimha
-	* Function Parameter: none
-	* Return Type: String
-	* Function Description: this function when called pops an integer from the top of the bullet_stack.
-	*/
-	public String fireBullet(){
-		String message;
-		if(top == -1){
-			message = "#Stack Empty";
-			return message;
-		}
-		message = bullet_stack.get(top)+"#Successful";
-		top--;
-		return message;
-	}
-	
-	/*
-	* Function Name: getMagazineStack
-	* Author: Narasimha
-	* Function Parameter: none
-	* Return Type: Vector object
-	* Function Description: this function creates a clone of the stack that is currently being used.
-	*/
-	public Vector<Integer> getMagazineStack(){
-		@SuppressWarnings("UseOfObsoleteCollectionType")
-		Vector<Integer> object_clone = (Vector<Integer>) bullet_stack.clone();
-		return object_clone;
-	}
 }
